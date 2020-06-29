d48c76ea1c21d6e6cfb7f991493ed94f916ce673
manishngk99 <manishngk99@gmail.com>
2020-06-29 11:56:58 +0545

    post job modified to admin page

---
+++ resources/views/layout/admin_layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+   @include('layout.partials.head')
+ </head>
+ <body>
+@include('layout.partials.admin.admin_header')
+@yield('content')
+@include('layout.partials.footer-scripts')
+ </body>
+</html>
