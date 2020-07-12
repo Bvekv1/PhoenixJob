8e113d976e8608835b72b7d8d78f508a3ca44bd0
suwalbikesh <suwalbikezh@gmail.com>
2020-07-12 14:17:24 +0545

    api connection for job update

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -123,5 +123,80 @@ class JobController extends Controller
         }
     }
 
+    public function job_edit_page($jobId){
+
+        $response = Http::get('http://localhost:4000/jobDetails/'. $jobId);
+        $data = json_decode($response->body());
+//        dd($data);
+        if ($data !== []){
+            return view('post_Job',['edit'=>$data]);
+        } else {
+            return redirect()->back()->withErrors(['error'=>'No data']);
+        }
+    }
+
+    public function job_edit(request $request){
+        $this->validate($request,[
+            'job_title' => 'required',
+            'experience' => 'required',
+            'level' => 'required',
+            'position' => 'required',
+            'jobType' => 'required',
+            'salary' => 'required',
+            'education' => 'required',
+            'location' => 'required',
+            'applyBefore' => 'required',
+            'jobDescription' => 'required',
+            'jobQualification' => 'required',
+            'jobHours' => 'required',
+            'benefits' => 'required',
+            'expected' => 'required'
+        ]);
+
+        $job_title = $request->input('job_title');
+        $experience = $request->input('experience');
+        $level = $request->input('level');
+        $position = $request->input('position');
+        $jobType = $request->input('jobType');
+        $salary = $request->input('salary');
+        $education = $request->input('education');
+        $location = $request->input('location');
+        $applyBefore = $request->input('applyBefore');
+        $jobDescription = $request->input('jobDescription');
+        $jobQualification = $request->input('jobQualification');
+        $expected = $request->input('expected');
+        $jobHours = $request->input('jobHours');
+        $benefits = $request->input('benefits');
+
+        $userToken = session()->get('usertoken');
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer '.$userToken
+        ])->put('http://localhost:4000/jobinfoUpdate/'.$request->input('jobId'), [
+            'jobTitle' => $job_title,
+            'experience' => $experience,
+            'level' => $level,
+            'positions' => $position,
+            'jobType' => $jobType,
+            'salary' => $salary,
+            'education' => $education,
+            'location' => $location,
+            'applyBefore' => $applyBefore,
+            'jobDescription' => $jobDescription,
+            'jobQualification' => $jobQualification,
+            'jobHours' => $jobHours,
+            'benefits' => $benefits,
+            'expected' => $expected
+        ]);
+
+        $data = json_decode($response->body());
+//        dd($data);
+        if ($data->status == 200 && $data->message === 'Job info was successfully updated'){
+            return back()->withErrors(['message' => $data->message]);
+        }
+        else {
+            return back()->withErrors(['error' => 'job not update']);
+        }
+    }
+
 
 }
